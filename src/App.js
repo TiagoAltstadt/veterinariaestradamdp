8eb37c6ae4f131850cdefea975eeca29f27cc1c7
TiagoAltstadt <tiagoaltstadt@gmail.com>
2020-11-12 12:35:40 -0300

    first commit, just created header and deleted some unnecesary files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './public/css/styles.css';
+import Header from './components/header.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <div className="Header">
+      
+      <section className="headerTitle">
+        <h1><a href='/'>Veterinaria Estrada</a></h1>
+      </section>
+      <section className="headerNavbar">
+        <div className="headerNavbarOption"> <a href='/'> Home </a></div>
+        <div className="headerNavbarOption"> <a href='/'> Turnos</a></div>
+        <div className="headerNavbarOption"> <a href='/'> Acerca-de</a></div>
+        <div className="headerNavbarOption"> <a href='/'> Contacto</a></div>
+        <div className="headerNavbarOption"> <a href='/'> Banana</a></div>
+      </section>
+
+    </div>
+  );
+}
+
+export default Header;
